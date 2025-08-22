43af447214e0dc94e9cc2fd3700a994b6b45f7fe
UmDangar <umesh.dangar@metizsoft.com>
2025-08-22 10:49:03 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,8 +6,8 @@ echo 'if [ -f "app.js" ]; then' >> scripts/start_server.sh
 echo '    nohup node app.js > app.log 2>&1 &' >> scripts/start_server.sh
 echo 'elif [ -f "server.js" ]; then' >> scripts/start_server.sh
 echo '    nohup node server.js > app.log 2>&1 &' >> scripts/start_server.sh
-echo 'elif [ -f "index.js" ]; then' >> scripts/start_server.sh
-echo '    nohup node index.js > app.log 2>&1 &' >> scripts/start_server.sh
+echo 'elif [ -f "index.ts" ]; then' >> scripts/start_server.sh
+echo '    nohup node index.ts > app.log 2>&1 &' >> scripts/start_server.sh
 echo 'else' >> scripts/start_server.sh
 echo '    nohup npm start > app.log 2>&1 &' >> scripts/start_server.sh
 echo 'fi' >> scripts/start_server.sh
